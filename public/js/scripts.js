999cf1359ee3be55266edc208b12a03cc7813eae
Sathya Ram <sathyatheram@gmail.com>
2018-10-01 23:14:30 -0400

    added js logic to actually append new items into the ul without refreshing page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,18 @@
 // For Random Javascript
+function makeListItem(item) {
+    const unorderedList = document.querySelector("#item-wrapper")
+    const listItem = `
+    <li style="background: ${item.color}">
+        <a class="website-wrapper" target="_blank" href="${item.link}">
+            <div class="title">${item.title}</div>
+            <div class="link">${item.link}</div>
+            <div class="tags">${item.tags}</div>
+            <div class="desc">${item.description}</div>
+        </a>
+    </li>
+    `
+    unorderedList.appendChild(listItem);
+}
 
 // Ajax Request
 document.querySelector('#itemform').addEventListener("submit", function(e) {
@@ -24,7 +38,9 @@ document.querySelector('#itemform').addEventListener("submit", function(e) {
     .then(response => {
         console.log(response)
         response.json().then(res => {
-            console.log(res);
+            if (res.ok) {
+                makeListItem();
+            } else alert('Error adding item');
         });
     })
     .catch(err => {console.log(err) })
