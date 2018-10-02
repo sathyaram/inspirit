26fef7f1be74be57adaea35f38471876f06a31f3
Sathya Ram <sathyatheram@gmail.com>
2018-10-01 22:49:52 -0400

    items now render on frontpage

--- routes/getindex.js
+++ routes/getindex.js
@@ -1,35 +1,9 @@
 // Path: / - Method: GET - Action: #index - Desc: Shows All Grid Items
 const router = require('express').Router();
+const { Item } = require('../models/Item')
 
 router.get('/', (req, res) => {
-    // Items need to come from database
-    let items = [
-    {
-        title: "Sean Halpin",
-        link: "http://seanhalpin.io",
-        tags: ["web", "design", "sexy"],
-        description: "A nice portfolio",
-        user: "sathyaram",
-        color: "#4ab19a"
-    },
-    {
-        title: "Jenny Johannesson",
-        link: "http://www.jennyjohannesson.com/",
-        tags: ["product", "designer", "minecraft"],
-        description: "a digital builder",
-        user: "sathyaram",
-        color: "#ff086c"
-    },
-    {
-        title: "Tobias Anhlin",
-        link: "http://tobiasanhlin.com",
-        tags: ["product", "designer", "minecraft"],
-        description: "a digital builder",
-        user: "sathyaram",
-        color: "#1f4954"
-    }
-    ]
-    res.render('index', { items });
+    Item.find().then(items => res.render('index', { items }));
 })
 
 module.exports = router;
